ce1e0ff288e5c47bdadd78055c82c918a527d2a6
Arni <arni.naurloth@gmail.com>
2016-11-16 23:26:20 +0100

    Commit basic pom structure

---
+++ src/main/java/tptea/sdrgmts/model/Character.java
@@ -0,0 +1,8 @@
+package tptea.sdrgmts.model;
+
+/**
+ * Reprentation of a Shadowrun character with his stats, skills and background.
+ */
+public class Character {
+    
+}
---
+++ src/main/java/tptea/sdrgmts/util/json/JSONLoader.java
@@ -0,0 +1,8 @@
+package tptea.sdrgmts.util.json;
+
+/**
+ * Interface to Load JSON Files filled with descriptions. 
+ */
+public class JSONLoader {
+    
+}
