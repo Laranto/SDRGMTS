bfc7b21fee65781f0e12a9a5db2443e873f66aad
Arni <arni.naurloth@gmail.com>
2016-11-25 23:57:09 +0100

    Added simple json and an example call

--- src/main/java/tptea/sdrgmts/util/json/JSONLoader.java
+++ src/main/java/tptea/sdrgmts/util/json/JSONLoader.java
@@ -1,8 +1,10 @@
 package tptea.sdrgmts.util.json;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
  * Interface to Load JSON Files filled with descriptions. 
@@ -10,13 +12,20 @@ import jdk.nashorn.internal.parser.JSONParser;
 public class JSONLoader {
     
     private File rootFile;
+    private JSONParser parser;
     
     public JSONLoader() {
         try {
             rootFile = new File("resources"+File.separator+"main.json");
+            parser = new JSONParser();
+            System.out.println(rootFile);
             if (!rootFile.exists()) {
                 throw new IOException("main.json does not exist!");
             }
+
+            JSONObject rootFileObject = (JSONObject) parser.parse(new FileReader(rootFile));
+            System.out.println(rootFileObject.toString());
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
