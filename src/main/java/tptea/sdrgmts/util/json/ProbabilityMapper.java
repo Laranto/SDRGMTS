d8083302c832d309592b73d848abd63617579edf
Koen van der Keijl <kvanderkeyl@gmail.com>
2016-11-26 01:21:28 +0100

    made probability mapper generic

--- src/main/java/tptea/sdrgmts/util/json/ProbabilityMapper.java
+++ src/main/java/tptea/sdrgmts/util/json/ProbabilityMapper.java
@@ -3,20 +3,24 @@ package tptea.sdrgmts.util.json;
 import java.util.Random;
 import java.util.TreeMap;
 
-public class ProbabilityMapper {
-	TreeMap<Integer,String> probabilityMap;
-	Integer maxValue = 0;
-	
-	public ProbabilityMapper() {
-		probabilityMap = new TreeMap<>();
-	}
-	
-	public void put(int key, String value) {
-		probabilityMap.put(maxValue, value);
-		maxValue += key;
-	}
-	
-	public String get(Random rand){
-		return probabilityMap.floorEntry(rand.nextInt(maxValue)).getValue();
-	}
+public class ProbabilityMapper<T> 
+{
+    TreeMap<Integer,T> probabilityMap;
+    Integer maxValue = 0;
+
+    public ProbabilityMapper() 
+    {
+        probabilityMap = new TreeMap<>();
+    }
+
+    public void put(int key, T value) 
+    {
+        probabilityMap.put(maxValue, value);
+        maxValue += key;
+    }
+
+    public T get(Random rand)
+    {
+        return probabilityMap.floorEntry(rand.nextInt(maxValue)).getValue();
+    }
 }
